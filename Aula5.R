3d172520240ff9216747c3614a98f2312ad4eaa0
Otto Tavares <otavares93@gmail.com>
2025-02-11 21:30:41 -0300

    iniciando o renv, instalando o tidyverse seguido da atualização da renv

--- Aula5.R
+++ Aula5.R
@@ -1,1 +1,7 @@
 library(renv)
+#iniciando o ambiente R para registro de pacotes
+renv::init()
+#exemplo de instalação de pacote
+install.packages('tidyverse')
+#atualizando o mesmo ambiente após instalação do pacote exemplo
+renv::snapshot()
